375cb4299b7b56262cab672846fadd23635c028c
Jesper Lundgren <koudelkaa@gmail.com>
2022-09-15 14:15:52 +0200

    Bypass worker parsing in response streams. Enable direct connection between DO & Client.

--- src/utils/createHelixRequest.ts
+++ src/utils/createHelixRequest.ts
@@ -1,12 +1,13 @@
-export const createHelixRequest = async (request: Request) => {
+import {type Request as HelixRequest} from 'graphql-helix'
+export const createHelixRequest = async (request: Request): Promise<HelixRequest> => {
   const url = new URL(request.url)
   const query = Object.fromEntries(new URLSearchParams(url.search))
 
   const body =
-    request.method === 'POST' ? await request.clone().json() : undefined
+    request.method === 'POST' ? await request.json() : undefined
 
   return {
-    body,
+    body: body,
     headers: request.headers,
     method: request.method,
     query,
